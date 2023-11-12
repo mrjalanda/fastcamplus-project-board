a6948f3e2e02b7e3bee72a64077788ebda1c7c81
mrjalanda <berichldw@gmail.com>
2023-11-12 18:25:44 +0900

    project init

---
+++ src/main/java/com/fastcamplus/projectboard/FastCamplusProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.fastcamplus.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCamplusProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastCamplusProjectBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastcamplus/projectboard/FastCamplusProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcamplus.projectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastCamplusProjectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
