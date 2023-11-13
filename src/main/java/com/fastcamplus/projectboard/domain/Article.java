13a1a8723139452396c1ef487f255a045d6063ba
mrjalanda <berichldw@gmail.com>
2023-11-13 14:35:49 +0900

    source modify

--- src/main/java/com/fastcamplus/projectboard/domain/Article.java
+++ src/main/java/com/fastcamplus/projectboard/domain/Article.java
@@ -7,13 +7,13 @@ import java.time.LocalDateTime;
 @Builder
 public class Article {
 
-    private Long id;
-    private String title;
-    private String content;
-    private String hashtag;
-    private LocalDateTime createdAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    private Long id; //게시글 고유번호
+    private String title; //제목
+    private String content; //내용
+    private String hashtag; //해시 태그
+    private LocalDateTime createdAt; //작성일
+    private String createdBy; //작성자
+    private LocalDateTime modifiedAt; //수정일
+    private String modifiedBy; //수정자
 
 }
